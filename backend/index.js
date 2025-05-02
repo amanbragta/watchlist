82de0b3c979c7d8ddf8f98fc9e0603a48ffdac7c
amanbragta <amanbragta@gmail.com>
2025-05-02 18:57:48 +0530

    modified session

--- backend/index.js
+++ backend/index.js
@@ -26,7 +26,8 @@ app.use(
     resave: false,
     cookie: {
       maxAge: 60000 * 60 * 24,
-      sameSite: "none",
+      secure: true,
+      sameSite: "None",
     },
     store: MongoStore.create({
       client: mongoose.connection.getClient(),
